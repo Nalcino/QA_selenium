dd47b87363157456c20685ffe27c45a9e6019c51
Paulo Henrique Nalcino <phnalcino@gmail.com>
2025-07-23 14:32:07 -0300

    Delete

--- github_helper_lib_final/github_helper/__init__.py
+++
@@ -1,2 +0,0 @@
-from .client import GitHubClient
-from .banco import bancoGithub
--- github_helper_lib_final/github_helper/client.py
+++
@@ -1,7 +0,0 @@
-from .auth.chain import resolve_token
-from .base import Git
-
-class GitHubClient(Git):
-    def __init__(self, personal_token: str | None = None):
-        token = resolve_token(personal_token)
-        super().__init__(token)
